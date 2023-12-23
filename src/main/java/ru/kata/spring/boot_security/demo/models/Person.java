a64a5e1b13492660bcf40dc4c97d4bf4ea2c4e09
Bogdan Bulgakov <Bog.bulgakov@yandex.ru>
2023-12-23 17:55:10 +0300

    создал новые папки, классы и интерфейсы, создал модель с новыми полями и подключился с ней к новой БД, пока все работает

---
+++ src/main/java/ru/kata/spring/boot_security/demo/models/Person.java
@@ -0,0 +1,109 @@
+package ru.kata.spring.boot_security.demo.models;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "Person")
+public class Person {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotEmpty(message = "имя не должно быть пустым")
+    @Size(min = 2, max = 60, message = "имя должно быть от 2 до 60 символов")
+    @Column(name = "first_name")
+    private String firstName;
+
+    @NotEmpty(message = "фамилия не может быть пустой")
+    @Size(min = 2, max = 60, message = "фамилия должна быть от 2 до 60 символов")
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Min(value = 0, message = "возраст не может быть отрицательным")
+    @Column(name = "age")
+    private int age;
+
+    @Email
+    @Column(name = "email")
+    private String email;
+
+    @NotEmpty(message = "пароль не может быть пустым")
+    @Size(min = 4, message = "пароль должен быть от 4 символов")
+    @Column(name = "password")
+    private String password;
+
+    public Person(String firstName, String lastName, int age, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.email = email;
+    }
+
+    public Person() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/ru/kata/spring/boot_security/demo/services/PersonDetailsService.java
@@ -0,0 +1,15 @@
+//package ru.kata.spring.boot_security.demo.services;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//import ru.kata.spring.boot_security.demo.repositiries.PeopleRepository;
+//
+//@Service
+//public class PersonDetailsService {
+//    private final PeopleRepository peopleRepository;
+//
+//    @Autowired
+//    public PersonDetailsService(PeopleRepository peopleRepository) {
+//        this.peopleRepository = peopleRepository;
+//    }
+//}
